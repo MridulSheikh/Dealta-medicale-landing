6ed388258d678c612610e0c8537f05b50de14935
MridulSheikh <prince9nazir@gmail.com>
2022-02-05 23:16:31 +0600

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,13 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false,
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {
-      spacing: {
-        13: "3.25rem",
-        15: "3.75rem",
-        128: "32rem",
-        144: "36rem",
-      },
-      colors: {
+      colors:{
         primary: "#2687B4",
-        malbec: "#3D1A24",
-        tangerine: "#2687B4",
-        aqua: "#9EE0D6",
-        teal: "#1C3D40",
-      },
-      zIndex: {
-        1001: 1001,
-      },
+      }
     },
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-};
+}
